896452cb53200359f6d38ef8db639159bf47cd1a
Gareth Pledger <garpled@chbc.school.nz>
2023-02-07 12:46:23 +1300

    Add project files.

---
+++ SwimApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwimApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
